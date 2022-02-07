97faf06ea90e6aaa4168037bffd9979c9ad26ae7
Carrie Miskin <clove40@gmail.com>
2022-02-07 16:22:27 -0500

    commit tree 3db92ac4e74c9a065a0df299741358b41b817082

---
+++ index.js
@@ -0,0 +1,48 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add(x, y) {
+    console.log(add(1, 80));
+  }
+add (1 + 80);
+
+function subtract(x, y) {
+  console.log(subtract(60, 40));
+}
+subtract (60 - 40);
+
+function multiply(x, y) {
+  console.log(multiply(2, 3.4));
+}
+multiply (2 * 3.4);
+
+function divide(x, y) {
+  console.log(divide(5.0, 2.5));
+}
+divide (5.0 / 2.5);
+
+function multiply(a, b){
+  return (a * b);
+}
+
+function divide(a, b){
+  return (a / b);
+}
+
+function increment(a){
+  return (a + 1);
+}
+
+function decrement(a){
+  return (a - 1);
+}
+
+function makeInt(a){
+  return parseInt(a, 10);
+}
+
+function preserveDecimal(a){
+  return parseFloat(2.22);
+}
